7ed337f74ae5b8cb64e4d6c41765f2e8c6c78cfb
Kárándi Tamás <tkarandi@gmail.com>
2016-04-23 23:48:34 +0200

    Add solution for Problem  19

---
+++ src/net/projecteuler/ProjectEuler019.java
@@ -0,0 +1,20 @@
+package net.projecteuler;
+
+public class ProjectEuler019 {
+
+	private final static int[] DAYS 		= new int[] {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	private final static int[] DAYS_LEAP	= new int[] {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+	public static void main(String[] args) {
+		int day = 2; //tuesday 1901, Jan 1
+		int count = 0;
+		for (int year = 1901; year <= 2000; year++) {
+			int[] days = year % 4 == 0 ? DAYS_LEAP : DAYS;
+			for (int month = 0; month < days.length; month++) {
+				if (day % 7 == 0) { count++; }
+				day = (day + days[month]) % 7;
+			}
+		}
+		System.out.println(count);
+	}	
+}
