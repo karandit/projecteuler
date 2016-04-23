afa587fb8e1f93232e7aa9b68affc1fd2dc2c0e8
Kárándi Tamás <tkarandi@gmail.com>
2016-04-23 23:48:51 +0200

    Add solution for Problem  26

---
+++ src/net/projecteuler/ProjectEuler026.java
@@ -0,0 +1,37 @@
+package net.projecteuler;
+
+public class ProjectEuler026 {
+
+	public static void main(String[] args) {
+		int dMax = 2;
+		int lengthMax = 0;
+		for (int d = 2; d < 1000; d++) {
+			int l = recurringCycle(d);
+			if (l > lengthMax) {
+				lengthMax = l;
+				dMax = d;
+			}
+		}
+		System.out.println(dMax);
+	}
+
+	private static int recurringCycle(int d) {
+		int r = 10;
+		int[] fracts = new int[d]; 
+		int length = 0;
+		int mod = 0;
+//		StringBuilder sb = new StringBuilder("0.");
+		while (r != 0) {
+			mod = r % d;
+			if (fracts[mod] != 0) { break; } //repeating cycle
+//			sb.append(r /d);
+			fracts[mod] = ++length;
+			r = mod * 10;
+		}
+//		if (r != 0) {
+//			sb.append("(");
+//		}
+//		System.out.print(sb.toString());
+		return r == 0 ? 0 : (length - fracts[mod] +1);
+	}
+}
