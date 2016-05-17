8710585fc9d5c692cc476c201c3bac8f125c20be
Kárándi Tamás <tkarandi@gmail.com>
2016-05-18 00:01:27 +0200

    Apply some formatting on 060

--- src/main/java/net/projecteuler/ProjectEuler060.java
+++ src/main/java/net/projecteuler/ProjectEuler060.java
@@ -24,25 +24,27 @@ public class ProjectEuler060 {
 		boolean[] primes = getPrimes(THRESHOLD);
 		final Map<Integer, Set<Integer>> level1 = new HashMap<>();
 		final Map<Integer, Set<Integer>> level2 = new HashMap<>();
-		
+
 		for (int prime = 10; prime < primes.length; prime++) {
-			if (!primes[prime]) { continue; }
-			
+			if (!primes[prime]) {
+				continue;
+			}
+
 			List<Tuple<Integer, Integer>> separatedDigits = separatedDigits(prime);
 			for (Tuple<Integer, Integer> tuple : separatedDigits) {
 				if (!primes[tuple.getA()] || !primes[tuple.getB()]) {
 					continue;
 				}
-				
+
 				Integer a = tuple.getA();
 				Integer b = tuple.getB();
-					
-				Set<Integer> pairsA = getOrDefault(level1, a, new HashSet<>());;
+
+				Set<Integer> pairsA = getOrDefault(level1, a, new HashSet<>());
 				pairsA.add(b);
 				if (!level1.containsKey(b) || !level1.get(b).contains(a)) {
 					continue;
 				}
-				
+
 				Set<Integer> targetsA = getOrDefault(level2, a, new HashSet<>());
 				Set<Integer> targetsB = getOrDefault(level2, b, new HashSet<>());
 				targetsA.add(b);
@@ -55,7 +57,7 @@ public class ProjectEuler060 {
 				if (intersect.size() < 3) {
 					continue;
 				}
-				
+
 				for (Integer c : new HashSet<>(intersect)) {
 					if (!level2.containsKey(c)) {
 						continue;
@@ -64,7 +66,7 @@ public class ProjectEuler060 {
 					if (intersect.size() < 2) {
 						continue;
 					}
-					
+
 					for (Integer d : new HashSet<>(intersect)) {
 						if (!level2.containsKey(d)) {
 							continue;
@@ -84,12 +86,14 @@ public class ProjectEuler060 {
 		setA.retainAll(setB);
 		return setA;
 	}
-	
+
 	private static List<Tuple<Integer, Integer>> separatedDigits(int n) {
 		List<Tuple<Integer, Integer>> res = new LinkedList<>();
 		int[] digits = digits(n);
 		for (int sep = 1; sep < digits.length; sep++) {
-			if (digits[sep] == 0) { continue; }
+			if (digits[sep] == 0) {
+				continue;
+			}
 			int firstDigit = 0;
 			{
 				int multi = 1;
@@ -111,7 +115,7 @@ public class ProjectEuler060 {
 		return res;
 	}
 
-	public static<K, V> V getOrDefault(Map<K, V> map, K key, V value) {
+	public static <K, V> V getOrDefault(Map<K, V> map, K key, V value) {
 		if (!map.containsKey(key)) {
 			map.put(key, value);
 			return value;
