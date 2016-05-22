b307f2f534241f27472d2bc56a9c8cd0a3e300cd
Kárándi Tamás <tkarandi@gmail.com>
2016-05-22 18:29:49 +0200

    Use an alternate solution for Problem  98

--- src/main/java/net/projecteuler/ProjectEuler098.java
+++ src/main/java/net/projecteuler/ProjectEuler098.java
@@ -1,25 +1,29 @@
 package net.projecteuler;
 
-import static net.projecteuler.util.Util.fromDigits;
-import static net.projecteuler.util.Util.isSqrt;
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toMap;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 
-import net.projecteuler.util.Combinatorics;
 import net.projecteuler.util.Tuple;
+import net.projecteuler.util.Util;
 
 public class ProjectEuler098 {
 
+	private static final int UPPER_LIMIT = (int) Math.sqrt(1_000_000_000);
+	
 	public static void main(String[] args) {
 		long start = System.nanoTime();
 		System.out.println(solve_098());
@@ -27,144 +31,85 @@ public class ProjectEuler098 {
 	}
 
 	public static long solve_098() {
-		List<String> words = readWordsFromFile();
-		Map<String, List<String>> groups = groupWords(words);
-		Map<String, List<String>> rawAnagrams = filterAngarams(groups);
-		Map<List<Character>, Tuple< Tuple<List<String>, Set<Character>>, Tuple<List<int[]>, boolean[]>>> anagrams = normalizeKeys(rawAnagrams);
+		List<Tuple<String, String>> wordPairs = new LinkedList<>();
 		
-		Map<Integer, List<List<Integer>>> variationsCache = new HashMap<>();
-		long res = 0;
-		
-		for (Map.Entry<List<Character>, Tuple< Tuple<List<String>, Set<Character>>, Tuple<List<int[]>, boolean[]>>> entry : anagrams.entrySet()) {
-			List<Character> k = entry.getKey();
-			Tuple< Tuple<List<String>, Set<Character>>, Tuple<List<int[]>, boolean[]>> v = entry.getValue();
-		
-			List<int[]> allWordIdxs = v.getB().getA();
-			boolean[] zeros = v.getB().getB();
-			List<List<Integer>> variations = getVariations(variationsCache, k.size());
-			for (List<Integer> variation : variations) {
-				boolean skipVariation = false;
-				for (int zeroIdx = 0; zeroIdx < zeros.length; zeroIdx++) {
-					if (zeros[zeroIdx] && variation.get(zeroIdx) == 0) {
-						skipVariation = true;
-						break;
-					}
-				}
-				if (skipVariation) {
-					continue;
-				}
-				boolean allSqrt = true;
-				
-				long sqrtMax = 0;
-				for (int[] wordIdxs : allWordIdxs) {
-					int[] digits = applyVariation(variation, wordIdxs);
-					long sqrtCan = fromDigits(digits);
-					sqrtMax = Math.max(sqrtMax, sqrtCan);
-					if (!isSqrt(sqrtCan)) {
-						allSqrt = false;
-						break;
-					}
-				}
-				if (allSqrt) {
-					res = Math.max(res,  sqrtMax);
-				}
-			}
-		}
-		return res;
-	}
-
-	public static int[] applyVariation(List<Integer> variation, int[] wordIdxs) {
-		int[] digits = new int[wordIdxs.length];
-		int i = 0;
-		for (int wordIdx : wordIdxs) {
-			digits[i++] = variation.get(wordIdx);
-		}
-		return digits;
-	}
-	
-	private static List<List<Integer>> getVariations(Map<Integer, List<List<Integer>>> cache, Integer key) {
-		if (cache.containsKey(key)) {
-			return cache.get(key);
-		} else {
-			List<List<Integer>> variations = Combinatorics.variations(10, key);
-			cache.put(key, variations);
-			return variations;
-		}
-	}
-
-	private static Map<List<Character>, 
-	Tuple<
-	Tuple<List<String>, Set<Character>>,
-	Tuple<List<int[]>, boolean[]>
-	>> normalizeKeys(Map<String, List<String>> input) {
-		Map<List<Character>, 	Tuple<
-		Tuple<List<String>, Set<Character>>,
-		Tuple<List<int[]>, boolean[]>
-		>> res = new HashMap<>();
-		for (Map.Entry<String, List<String>> entry : input.entrySet()) {
-			List<Integer> distincCharsInt = entry.getKey().chars()
-					.distinct()
-					.boxed()
-					.collect(Collectors.toList());
-
-			
-			List<Character> distincChars = entry.getKey().chars()
-					.distinct()
-					.boxed()
-					.map(i -> new Character((char) i.intValue()))
-					.collect(Collectors.toList());
-			
-			Set<Character> firstChars = entry.getValue().stream()
-					.map(v -> v.charAt(0))
-					.collect(Collectors.toSet());
-			
-			//-----------
-			List<int[]> allIndices = new LinkedList<>();
-			for(String anagram : entry.getValue()) {
-				int[] indices = anagram.chars().map(c -> distincCharsInt.indexOf(c)).toArray();
-				allIndices.add(indices);
-			}
-			boolean[] firstCharsIdx = new boolean[entry.getValue().get(0).length()];
-			for(String anagram : entry.getValue()) {
-				firstCharsIdx[distincCharsInt.indexOf(new Integer(anagram.charAt(0)))] =  true;
-			}
-			
-			
-			Tuple<List<String>, Set<Character>> t1 = new Tuple<>(entry.getValue(), firstChars);
-			Tuple<List<int[]>, boolean[]> t2 = new Tuple<>(allIndices, firstCharsIdx);
-			res.put(distincChars, new Tuple<>(t1, t2));
-		}
-		return res;
-	}
-
-	private static Map<String, List<String>> filterAngarams(Map<String, List<String>> groups) {
-		Map<String, List<String>> anagrams = new HashMap<>();
-		for (Map.Entry<String, List<String>> groupEntry : groups.entrySet()) {
-			if (groupEntry.getValue().size() > 1) {
-				anagrams.put(groupEntry.getKey(), groupEntry.getValue());
-			}
-		}
-		return anagrams;
-	}
-
-	private static Map<String, List<String>> groupWords(List<String> words) {
-		Map<String, List<String>> groups = new HashMap<>();
-
-		for (String word : words) {	
+		readWordsFromFile("ProjectEuler_098.txt").stream()
+		.collect(groupingBy((String word) -> {
 			char[] keyAsArray = word.toCharArray();
 			Arrays.sort(keyAsArray);
-			String key = new String(keyAsArray);
-			if (!groups.containsKey(key)) {
-				groups.put(key, new LinkedList<>());
+			return new String(keyAsArray);
+		}))
+		.entrySet().stream()
+		.filter(e -> e.getValue().size() > 1)
+		.forEach(e -> {
+			List<String> words = e.getValue();
+			for (int i = 0; i < words.size() - 1; i++) {
+				for (int j = i + 1; j < words.size(); j++) {
+					wordPairs.add(new Tuple<>(words.get(i), words.get(j)));
+				}
 			}
-			List<String> group = groups.get(key);
-			group.add(word);
-		}
-		return groups;
+		});
+
+		Map<String, Long> fingerPrints = generateFingerPrints();
+		
+		return wordPairs.stream()
+		.map((Tuple<String, String> wordPair) -> generateFingerPrint(wordPair.getA(), wordPair.getB()))
+		.filter(fingerPrint -> fingerPrints.containsKey(fingerPrint))
+		.map(fingerPrint -> fingerPrints.get(fingerPrint))
+		.mapToLong(l -> (Long) l).max().getAsLong();
 	}
 
-	private static List<String> readWordsFromFile() {
-		try (InputStream is = ProjectEuler098.class.getResourceAsStream("ProjectEuler_098.txt");
+	public static Map<String, Long> generateFingerPrints() {
+		Map<String, Long> fingerPrints = new HashMap<>();
+		generateSquares().forEach((k, v) -> {
+				for (int i = 0; i < v.size() - 1; i++) {
+					for (int j = i + 1; j < v.size(); j++) {
+						Long long1 = v.get(i); 
+						Long long2 = v.get(j);
+						String fingerPrint = generateFingerPrintLong(long1, long2);
+						long max = Math.max(long1, long2);
+						if (fingerPrints.containsKey(fingerPrint)) {
+							Long act = fingerPrints.get(fingerPrint);
+							fingerPrints.put(fingerPrint, Math.max(max, act));
+						} else {
+							fingerPrints.put(fingerPrint, max);
+						}
+					}
+				}
+		});
+		return fingerPrints;
+	}
+
+	private static String generateFingerPrint(String s1, String s2) {
+		return asList(genMask(s1, s2), genMask(s2, s1)) .stream() .sorted() .collect(joining());
+	}
+
+	private static String generateFingerPrintLong(Long s1, Long s2) {
+		return asList(genMaskLong(s1, s2), genMaskLong(s2, s1)) .stream() .sorted() .collect(joining());
+	}
+
+	public static String genMask(String s1, String s2) {
+		StringBuilder sb = new StringBuilder();
+		for (char c : s2.toCharArray()) {
+			sb.append(s1.indexOf(c));
+		}
+		return sb.toString();
+	}
+
+	public static String genMaskLong(Long l1, Long l2) {
+		List<Integer> digits = new ArrayList<Integer>();
+		for (int i : Util.digits(l1)) {
+			digits.add(i);
+		}
+		StringBuilder sb = new StringBuilder();
+		for (int i : Util.digits(l2)) {
+			sb.append(digits.indexOf(i));
+		}
+		return sb.toString();
+	}
+
+	private static List<String> readWordsFromFile(String fileName) {
+		try (InputStream is = ProjectEuler098.class.getResourceAsStream(fileName);
 			BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
 				
 			List<String> words = new LinkedList<>();
@@ -178,6 +123,22 @@ public class ProjectEuler098 {
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	public static Map<String, List<Long>> generateSquares() {
+		return LongStream.rangeClosed(1, UPPER_LIMIT)
+		.map(i -> i * i)
+		.mapToObj(n -> (Long) n)
+		.collect(groupingBy(n -> {
+				int[] digits = Util.digits(n);
+				Arrays.sort(digits);
+				StringBuilder sb = new StringBuilder();
+				Arrays.stream(digits).forEach(x -> sb.append(x));
+				return sb.toString();
+		}))
+		.entrySet().stream()
+		.filter(e -> e.getValue().size() > 1)
+		.collect(toMap(e -> e.getKey(), e -> e.getValue()));
 	}
 
 }
